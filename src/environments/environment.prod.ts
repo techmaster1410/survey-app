43896e3f0605ba98ab0872108c4e678907c8760b
Akash Tripathi <tripathiaakash14@gmail.com>
2022-10-24 13:34:11 +0530

    New Stacks added M25, M30 and M1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
   production: true
 };
+
+findActiveEnv(activeDC : String){
+  if(activeDC == "M25"){
+    setActive("M25");
+  }
+  elif (activeDC == "M31"){
+    setActive("M31");
+  }
+  else{
+    setActive("M1");
+  }
+}
+    
